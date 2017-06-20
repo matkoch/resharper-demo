7ad4c47af5c517b72726260a41d3a073ad27650d
Matthias Koch <ithrowexceptions@gmail.com>
2017-06-20 21:38:34 +0200

    ++

--- Navigation/Test/NavigationIntegrationTest.cs
+++ Navigation/Test/NavigationIntegrationTest.cs
@@ -3,7 +3,15 @@ using System.Linq;
 
 namespace Demo.Navigation.Test
 {
+    [Subject(typeof(Car), typeof(Position))]
     public class NavigationIntegrationTest
     {
+    }
+
+    public class SubjectAttribute : Attribute
+    {
+        public SubjectAttribute (params Type[] subjects)
+        {
+        }
     }
 }
